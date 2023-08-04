d202a38248c1c9014bda531ba96f38e7e63e2ba2
Offering11 <lawaloffering@gmail.com>
2023-08-04 21:17:36 +0000

    don

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,40 +1,21 @@
 #!/usr/bin/python3
-"""
-Defines function that returns a list of lists of integers
-representing the Pascal's triangle of n
-"""
+'''A module for working with Pascal's triangle.
+'''
 
 
 def pascal_triangle(n):
-        """
-            Creates a list of lists of integers representing Pascal's triangle
-
-                parameters:
-                        n [int]:
-                                    the number of rows of Pascal's triangle to recreate
-
-                                        return:
-                                                [list of lists of ints]:
-                                                            representation of Pascal's triangle
-                                                                """
-                                                                    if type(n) is not int:
-                                                                                raise TypeError("n must be an integer")
-                                                                                triangle = []
-                                                                                    if n <= 0:
-                                                                                                return triangle
-                                                                                                previous = [1]
-                                                                                                    for row_index in range(n):
-                                                                                                                rowlist = []
-                                                                                                                        if row_index == 0:
-                                                                                                                                        rowlist = [1]
-                                                                                                                                                else:
-                                                                                                                                                                for i in range(row_index + 1):
-                                                                                                                                                                                    if i == 0:
-                                                                                                                                                                                                            rowlist.append(0 + previous[i])
-                                                                                                                                                                                                                            elif i == (row_index):
-                                                                                                                                                                                                                                                    rowlist.append(previous[i - 1] + 0)
-                                                                                                                                                                                                                                                                    else:
-                                                                                                                                                                                                                                                                                            rowlist.append(previous[i - 1] + previous[i])
-                                                                                                                                                                                                                                                                                                    previous = rowlist
-                                                                                                                                                                                                                                                                                                            triangle.append(rowlist)
-                                                                                                                                                                                                                                                                                                                return triangle
+    '''Creates a list of lists of integers representing
+    the Pascal's triangle of a given integer.
+    '''
+    triangle = []
+    if type(n) is not int or n <= 0:
+        return triangle
+    for i in range(n):
+        line = []
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                line.append(1)
+            elif i > 0 and j > 0:
+                line.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        triangle.append(line)
+    return triangle
